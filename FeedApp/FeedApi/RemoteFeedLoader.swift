0f0dd8ee3b4b86f99fed6acf17e9a728bb82cb52
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-04-13 21:31:08 +0300

    RemoteFeedLoader to production

---
+++ FeedApp/FeedApi/RemoteFeedLoader.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoader.swift
+//  FeedApp
+//
+//  Created by Pusca Ghenadie on 13/04/2019.
+//  Copyright © 2019 Pusca Ghenadie. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+    
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
--- FeedAppTests/RemoteFeedLoaderTests.swift
+++ FeedAppTests/RemoteFeedLoaderTests.swift
@@ -9,49 +9,35 @@
 import XCTest
 import FeedApp
 
-class RemoteFeedLoader {
-    private let client: HTTPClient
-    private let url: URL
-
-    init(client: HTTPClient, url: URL) {
-        self.client = client
-        self.url = url
-    }
-
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from: URL)
-}
-
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotLoad() {
-        let client = HTTPClientSpy()
-        let url = URL(string: "https://given-url.com")!
-        _ = RemoteFeedLoader(client: client, url: url)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client, url: url)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    private func makeSUT(url: URL = URL(string: "https://given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client, url: url)
+        return (sut, client)
+    }
+    
+    private
+    class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
