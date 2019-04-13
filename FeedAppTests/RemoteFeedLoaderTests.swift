638063e943abea69f55aa68c1c80e855c5712d3c
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-04-13 21:19:06 +0300

    Created HttpClient spy

---
+++ FeedAppTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,53 @@
+//
+//  FeedAppTests.swift
+//  FeedAppTests
+//
+//  Created by Pusca Ghenadie on 13/04/2019.
+//  Copyright © 2019 Pusca Ghenadie. All rights reserved.
+//
+
+import XCTest
+import FeedApp
+
+class RemoteFeedLoader {
+    private let client: HTTPClient
+
+    init(client: HTTPClient) {
+        self.client = client
+    }
+
+    func load() {
+        client.get(from: URL(string: "https://any-url.com")!)
+    }
+}
+
+class HTTPClient {
+    func get(from: URL) {
+        
+    }
+}
+
+class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+
+    override func get(from url: URL) {
+        requestedURL = url
+    }
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotLoad() {
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
+    }
+}
