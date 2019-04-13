4d85f89452d85086b8f918ed9b620740d6375b06
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-04-13 20:58:33 +0300

    Initial Commit

---
+++ FeedApp/FeedApp.h
@@ -0,0 +1,19 @@
+//
+//  FeedApp.h
+//  FeedApp
+//
+//  Created by Pusca Ghenadie on 13/04/2019.
+//  Copyright © 2019 Pusca Ghenadie. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FeedApp.
+FOUNDATION_EXPORT double FeedAppVersionNumber;
+
+//! Project version string for FeedApp.
+FOUNDATION_EXPORT const unsigned char FeedAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedApp/PublicHeader.h>
+
+
---
+++ FeedApp/FeedFeature/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  FeedApp
+//
+//  Created by Pusca Ghenadie on 13/04/2019.
+//  Copyright © 2019 Pusca Ghenadie. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
