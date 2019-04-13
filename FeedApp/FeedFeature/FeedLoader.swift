1fc51a08ec54803d150baf3685fef193d1d9538e
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-04-13 21:01:16 +0300

    Create the feed loader

---
+++ FeedApp/FeedFeature/FeedLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedLoader.swift
+//  FeedApp
+//
+//  Created by Pusca Ghenadie on 13/04/2019.
+//  Copyright © 2019 Pusca Ghenadie. All rights reserved.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: (LoadFeedResult) -> Void)
+}
