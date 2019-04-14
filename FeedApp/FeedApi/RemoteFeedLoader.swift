95f666f4be037907ce805e9c0033d67b5247b774
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-04-14 10:19:29 +0300

    Add feed maper

--- FeedApp/FeedApi/RemoteFeedLoader.swift
+++ FeedApp/FeedApi/RemoteFeedLoader.swift
@@ -41,10 +41,10 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                
-                if response.statusCode == 200, let decodedData = try? JSONDecoder().decode(Root.self, from: data){
-                    completion(.success(decodedData.items.map { $0.item }))
-                } else {
+                do {
+                    let items = try FeedItemsMapper.map(data, response)
+                    completion(.success(items))
+                } catch {
                     completion(.error(.invalidData))
                 }
             case .error:
@@ -54,20 +54,30 @@ public final class RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
-
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+private class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
     
-    var item: FeedItem {
-        return FeedItem(id: id,
-                        description: description,
-                        location: location,
-                        imageURL: image)
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(id: id,
+                            description: description,
+                            location: location,
+                            imageURL: image)
+        }
+    }
+
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        
+        return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
     }
 }
