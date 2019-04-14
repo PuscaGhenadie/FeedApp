da8129d97bb6cc4b67ccba976b601266ac92c30b
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-04-14 18:13:29 +0300

    Move the client to factory sut method.

--- FeedAppTests/FeedApi/URLSesssionHTTPClientTests.swift
+++ FeedAppTests/FeedApi/URLSesssionHTTPClientTests.swift
@@ -47,7 +47,7 @@ class URLSesssionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         wait(for: [exp], timeout: 1.0)
     }
     
@@ -56,7 +56,7 @@ class URLSesssionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
-        let sut = URLSessionHTTPClient()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for completion")
         sut.get(from: url) { result in
@@ -74,6 +74,10 @@ class URLSesssionHTTPClientTests: XCTestCase {
 
     // MARK: - Helpers
     
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
+
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
         
