8cdc735210fda69afd10e8b07d63b6f94bfb7b91
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-04-14 19:09:02 +0300

    Move the URLSessionHTTPClient to production

---
+++ FeedApp/FeedApi/URLSessionHttpClient.swift
@@ -0,0 +1,30 @@
+//
+//  URLSessionHttpClient.swift
+//  FeedApp
+//
+//  Created by Pusca Ghenadie on 14/04/2019.
+//  Copyright © 2019 Pusca Ghenadie. All rights reserved.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    private struct UnexpectedValuesRepresntation: Error {}
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.error(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.error(UnexpectedValuesRepresntation()))
+            }
+        }.resume()
+    }
+}
