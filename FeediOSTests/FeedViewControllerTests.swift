a9a748e95b33c021274c938d2378ef2770d24ec4
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-11-24 11:46:43 +0200

    Load feed when view did load

---
+++ FeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,51 @@
+//
+//  FeedViewControllerTests.swift
+//  FeediOSTests
+//
+//  Created by Pusca Ghenadie on 24/11/2019.
+//  Copyright © 2019 Pusca Ghenadie. All rights reserved.
+//
+
+import XCTest
+import UIKit
+
+final class LoaderSpy {
+    private(set) var loadCount = 0
+    
+    func load() {
+        loadCount += 1
+    }
+}
+
+final class FeedViewController: UIViewController {
+    private var loader: LoaderSpy?
+    
+    convenience init(loader: LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loader?.load()
+    }
+}
+
+final class FeedViewControllerTests: XCTestCase {
+    
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCount, 0)
+    }
+    
+    func test_viewLoaded_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(loader.loadCount, 1)
+    }
+}
