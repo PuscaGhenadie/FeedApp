939721223b71919f6d8255b665060d1147459f52
Ghenadie Pusca <pusca.ghenadie@gmail.com>
2020-01-03 13:31:23 +0200

    Move feed view controller to prod

---
+++ FeediOS/FeedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  test.swift
+//  FeediOS
+//
+//  Created by Pusca, Ghenadie on 1/3/20.
+//  Copyright © 2020 Pusca Ghenadie. All rights reserved.
+//
+
+import UIKit
+import FeedApp
+
+public final class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+    
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
