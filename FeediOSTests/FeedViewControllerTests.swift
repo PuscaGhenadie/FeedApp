526c7fe200f9470351ba01550e99c0f0956c2fc1
Ghenadie Pusca <pusca.ghenadie@gmail.com>
2020-01-03 13:01:37 +0200

    Refactor view controller to use prod FeedLoader

--- FeediOSTests/FeedViewControllerTests.swift
+++ FeediOSTests/FeedViewControllerTests.swift
@@ -10,25 +10,25 @@ import XCTest
 import UIKit
 import FeedApp
 
-final class LoaderSpy {
+final class LoaderSpy: FeedLoader {
     private(set) var loadCount = 0
     
-    func load() {
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
         loadCount += 1
     }
 }
 
 final class FeedViewController: UIViewController {
-    private var loader: LoaderSpy?
+    private var loader: FeedLoader?
     
-    convenience init(loader: LoaderSpy) {
+    convenience init(loader: FeedLoader) {
         self.init()
         self.loader = loader
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loader?.load()
+        loader?.load { _ in }
     }
 }
 
